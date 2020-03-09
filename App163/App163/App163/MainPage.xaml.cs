96b019bb215cfbb76248d8f0a2f9a310ce0b5f18
XfHua <jasonHuaxf@163.com>
2020-03-09 10:35:39 +0800

    Add files via upload

---
+++ App163/App163/App163/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App163
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public string previouspagevalue;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            previouspagevalue = "I'm previouspagevalue";
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //if you set the lblEndDT.Text = "someValue"; in the secondPage, there is no need to update it here
+            lblEndDT.Text = previouspagevalue;
+        }
+
+        private void btnOffline_Clicked(object sender, EventArgs e)
+        {
+            //Pass the parametere you need when you go to SecondPage 
+            //Navigation.PushAsync(new SecondPage(this, lblEndDT));
+
+            Navigation.PushAsync(new SecondPage(this, lblEndDT, MyImageButton));
+
+            string currentDT = DateTime.Now.ToString();
+            lblStartDT.Text = currentDT;
+
+        }
+    }
+}
---
+++ App163/App163/App163/SecondPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App163
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        Label MainPagelblEndDT;
+        MainPage mainPage;
+        ImageButton myImageBtn;
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        public SecondPage(MainPage mainP,Label lblEndDT)
+        {
+            InitializeComponent();
+
+            //Get the lblEndDT reference here
+            MainPagelblEndDT = lblEndDT;
+            //Get the MainPage reference here
+            mainPage = mainP;
+        }
+
+        public SecondPage(MainPage mainP, Label lblEndDT, ImageButton imageBtn)
+        {
+            InitializeComponent();
+
+            //Get the lblEndDT reference here
+            MainPagelblEndDT = lblEndDT;
+            //Get the MainPage reference here
+            mainPage = mainP;
+            //Get the ImageButton reference here
+            myImageBtn = imageBtn;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {           
+            string edt = DateTime.Now.ToString();
+
+            //Use it
+            MainPagelblEndDT.Text = edt;      
+            
+            mainPage.previouspagevalue = MainPagelblEndDT.Text;
+
+            //change the source of imageBtn
+            myImageBtn.Source = "Images1";
+            myImageBtn.IsEnabled = true;
+
+            Navigation.PopAsync();
+        }
+    }
+}
